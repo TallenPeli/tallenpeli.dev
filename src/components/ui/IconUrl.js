73efd92302ea1061c4ab0d78815259343b2bdd26
TallenPeli <tallen.pelissero@protonmail.com>
2025-08-14 23:47:51 -0400

    Fixed bug where IconURL elements would have their underline extend past the visible area

--- src/components/ui/IconUrl.js
+++ src/components/ui/IconUrl.js
@@ -1,27 +1,29 @@
 import "../../styles/components/ui/IconUrl.css";
 
 const IconUrl = ({ title, url, icon, iconSide }) => {
-  const Tag = url === "" ? "span" : "a";
+  const Selectable = url === "" ? "span" : "a";
   const props =
     url === ""
       ? { className: "icon-url" }
       : { href: url, className: "icon-url" };
 
-  return (
-    <Tag {...props}>
-      {iconSide === "left" ? (
-        <>
-          {icon}
-          <p>{title}</p>
-        </>
-      ) : (
-        <>
-          <p>{title}</p>
-          {icon}
-        </>
-      )}
-    </Tag>
-  );
+  if (!title) {
+    return <Selectable {...props}>{icon}</Selectable>;
+  } else if (iconSide === "left") {
+    return (
+      <Selectable {...props}>
+        {icon}
+        <p>{title}</p>
+      </Selectable>
+    );
+  } else {
+    return (
+      <Selectable {...props}>
+        <p>{title}</p>
+        {icon}
+      </Selectable>
+    );
+  }
 };
 
 export default IconUrl;
